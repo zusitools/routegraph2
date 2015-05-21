f8ef33046282b7f5b643f80065539f702df12937
Johannes <zusitools@web.de>
2015-05-21 09:57:08 +0200

    Vereinfache Code zum Erzeugen von Streckensegmenten

--- view/graphicsitems/streckensegmentitem.cpp
+++ view/graphicsitems/streckensegmentitem.cpp
@@ -16,35 +16,32 @@ StreckensegmentItem::StreckensegmentItem(StreckenelementUndRichtung start,
     if (start.richtung == Streckenelement::RICHTUNG_NORM)
     {
         path.moveTo(startelem->p1.x, startelem->p1.y);
-        path.lineTo(startelem->p2.x, startelem->p2.y);
     }
     else
     {
         path.moveTo(startelem->p2.x, startelem->p2.y);
-        path.lineTo(startelem->p1.x, startelem->p1.y);
     }
 
-    if (start.hatNachfolger()) {
-        auto cur = start.nachfolger();
-        while (!istSegmentStart(cur)) {
-            if (cur.richtung == Streckenelement::RICHTUNG_NORM) {
-                path.lineTo(cur.streckenelement.lock()->p2.x, cur.streckenelement.lock()->p2.y);
-            } else {
-                path.lineTo(cur.streckenelement.lock()->p1.x, cur.streckenelement.lock()->p1.y);
-            }
-
-            if (!cur.hatNachfolger())
-            {
-                break;
-            }
-            auto next = cur.nachfolger();
-            if (next.hatVorgaenger() && next.vorgaenger().streckenelement.lock() != cur.streckenelement.lock())
-            {
-                break;
-            }
-            cur = next;
+    auto cur = start;
+    do {
+        auto curelem = cur.streckenelement.lock();
+        if (cur.richtung == Streckenelement::RICHTUNG_NORM) {
+            path.lineTo(curelem->p2.x, curelem->p2.y);
+        } else {
+            path.lineTo(curelem->p1.x, curelem->p1.y);
         }
-    }
+
+        if (!cur.hatNachfolger())
+        {
+            break;
+        }
+        auto next = cur.nachfolger();
+        if (next.hatVorgaenger() && next.vorgaenger().streckenelement.lock() != curelem)
+        {
+            break;
+        }
+        cur = next;
+    } while (!istSegmentStart(cur));
 
     this->setPath(path);
 
